a9a7aec53f5278411e642c88b04161931d84872f
Veit Pulver <veitpulver@gmx.de>
2024-10-27 20:27:41 +0100

    mit Besuchercount

--- website/views.py
+++ website/views.py
@@ -4,4 +4,4 @@ from .models import VisitorsCount
 def home(request):
 
     visitor_count = VisitorsCount.objects.get(id=1).count
-    return render(request, 'user_count.html', {'visitor_count': visitor_count})
+    return render(request, 'home.html', {'visitor_count': visitor_count})
