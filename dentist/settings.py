4288ce2cf49eaae4aefa7a23588d0234b0e4a97e
Veit Pulver <veitpulver@gmx.de>
2024-10-31 20:50:21 +0100

    Update database configuration to PostgreSQL

--- dentist/settings.py
+++ dentist/settings.py
@@ -20,7 +20,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -32,11 +31,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'website',
+    'website.apps.WebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,17 +76,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dentist.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -108,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -120,7 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -131,13 +122,12 @@ STATICFILES_DIR = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 django_heroku.settings(locals())
--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import VisitorsCount
 
+def about(request):
+    return render(request, 'website/about.html',
+                  {'title': '💪 Mopssport About 👯‍♂'})
+
+
 def home(request):
 
     visitor_count = VisitorsCount.objects.get(id=1).count
-    return render(request, 'home.html', {'visitor_count': visitor_count})
+    return render(request, 'website/home.html', {'visitor_count': visitor_count, 'title': '💪 Mopssport Workout Site 👯‍♂'})
